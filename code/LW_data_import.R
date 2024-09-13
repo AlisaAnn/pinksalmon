1e559902d182f79a1af22ab561929dae2e48faf2
Alisa Abookire <alaskacor@gmail.com>
2024-09-12 16:29:41 -0800

    add 2024 length data and inport it

--- code/LW_data_import.R
+++ code/LW_data_import.R
@@ -12,9 +12,9 @@ head(pinkLW)
 
 tail(pinkLW)
 ##to check that last rows are real data. if need to remove these 3 rows
-#pinkLW <- filter(pinkLW, !is.na(Total_length))
+pinkLW <- filter(pinkLW, !is.na(Length))
 head(pinkLW)
-tail(pinkLW) #you can see it worked because now 759 obs
+tail(pinkLW) #you can see it worked 
 
 #need to remove years 2017 and 2021
 #codcond1<-filter(codcond1,year=="2018"|year=="2019" | year == "2020")
@@ -22,6 +22,7 @@ tail(pinkLW) #you can see it worked because now 759 obs
 #distinct(codcond1, year)
 
 
+distinct(pinkLW,Year) #years 2021 - 2024 
 distinct(pinkLW,Month)
 # When we boxplot this, we notice that the factors in month are alphabetical. 
 #we want them to be sequential, so need to create a new column "Month" w ordered factor
@@ -31,10 +32,10 @@ pinkLW <- mutate(pinkLW, Month = fct_relevel(Month, c("May", "June", "July", "Au
 str(pinkLW) # we can see now that the factors are ordered correctly 
 
 ##  CAN ADD CPUE DATA FOR PINKS NEXT
-PC <- read_csv("data/pink_cpue.csv")
-head(PC)
-distinct(PC, bay)
-distinct(PC,year)
-distinct(PC,month)
-tail(PC)
+#PC <- read_csv("data/pink_cpue.csv")
+#head(PC)
+#distinct(PC, bay)
+#distinct(PC,year)
+#distinct(PC,month)
+#tail(PC)
 
